0174c7fb683ed485c13b61c43be2d5087210eee1
Fruit-Snacks <kyle.mclean@pop.belmont.edu>
2017-01-19 11:44:54 -0600

    added my sites to scapewithrequests.py

--- ScrapeWithRequests.py
+++ ScrapeWithRequests.py
@@ -5,7 +5,7 @@ today = str(datetime.datetime.now()).split(' ')[0]
 
 sites = {'craigslist': 'https://nashville.craigslist.org/search/apa',
          'apartments.com':'https://www.apartments.com/nashville-tn/',
-         'rent.com':'http://www.rent.com/tennessee/nashville-houses/'}
+         'Rent.com':'http://www.rent.com/tennessee/nashville-houses/'}
 
 for name, link in sites.items():
     response = requests.get(link)
