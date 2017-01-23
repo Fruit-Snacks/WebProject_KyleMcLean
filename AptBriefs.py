1519450c2687533e51452a7c049438719fa612ab
Fruit-Snacks <kyle.mclean@pop.belmont.edu>
2017-01-22 22:22:34 -0600

    Apartments.com scrape shell

---
+++ AptBriefs.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Scrape APNewsBriefs with requests
+urlAPNewsBriefs = 'https://www.apartments.com/nashville-tn/?bb=qrwyv87s3Ig4_x5mY'
+pageAPNewsBriefs = requests.get(urlAPNewsBriefs)
+
+# Prepare for parsing APNewsBriefs with BeautifulSoup
+soupAPNewsBriefs = BeautifulSoup(pageAPNewsBriefs.content, 'lxml')
+
+position = soupAPNewsBriefs.find('div', class_='location')
+location = position.string
+brief = soupAPNewsBriefs.find('div', class_='apartmentRentRollupContainer')
+price = brief.find('span', class_='altRentDisplay').string
+size = brief.find('span', class_='unitLabel').string
+contact = soupAPNewsBriefs.find('div', class_='phone')
+phone = contact.find('span').string
+postTime = soupAPNewsBriefs.find('span', class_='lastUpdated')
+time = postTime.find('span').string + ' ago'
+
+
+print(location)
+print(price)
+print(size)
+print(phone)
+print(time)
