e99d4751974092b61a57cd0829f0250e70c2b5d7
Fruit-Snacks <kyle.mclean@pop.belmont.edu>
2017-01-23 22:39:12 -0600

    rent.com scrape shell

---
+++ RentBrief.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Scrape APNewsBriefs with requests
+urlAPNewsBriefs = 'http://www.rent.com/tennessee/nashville-houses/74-491'
+pageAPNewsBriefs = requests.get(urlAPNewsBriefs)
+
+# Prepare for parsing APNewsBriefs with BeautifulSoup
+soupAPNewsBriefs = BeautifulSoup(pageAPNewsBriefs.content, 'lxml')
+
+
+brief = soupAPNewsBriefs.find('p', class_='prop-address')
+locationRegion = brief.find('span', itemprop='addressRegion')
+locationRegion.replace_with('')
+locationState = brief.find('span')
+locationState.replace_with('')
+location = brief.find('span').string
+cost = soupAPNewsBriefs.find('p', class_='prop-rent')
+price = cost.find('span').string
+size = soupAPNewsBriefs.find('span', class_='prop-beds bullet-separator').string
+phone = soupAPNewsBriefs.find('strong', class_='tel').string
+
+
+
+print(location)
+print(price)
+print(size)
+print(phone)
+
