c30beef4377bc956c17d2bc600e21049a6b50cc8
Fruit-Snacks <kyle.mclean@pop.belmont.edu>
2017-01-23 22:39:34 -0600

    rent.com scrape shell

--- RentBrief.py
+++ RentBrief.py
@@ -20,7 +20,7 @@ price = cost.find('span').string
 size = soupAPNewsBriefs.find('span', class_='prop-beds bullet-separator').string
 phone = soupAPNewsBriefs.find('strong', class_='tel').string
 
-#jj
+#jjj
 
 print(location)
 print(price)
